f61c87b603a854d49c248bba9938ce83105bc123
Zeyad Mohamed <ziadmohomar123@gmail.com>
2025-10-26 14:47:57 +0300

    add: handle only get requests with static response

--- src/main/java/org/example/HttpServer.java
+++ src/main/java/org/example/HttpServer.java
@@ -17,15 +17,29 @@ public class HttpServer {
         Socket clientSocket = serverSocket.accept();
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         out = new PrintWriter(clientSocket.getOutputStream(), true);
+//        System.out.println(in.readLine());
 
-        out.println("HTTP/1.1 200 OK");
-        out.println("Content-Type: text/plain");
-        out.println();
-        out.println("Hello client");
+        getMehtod(in.readLine(), out);
         clientSocket.close();
-
     }
 
 
+    public void getMehtod(String reqString, PrintWriter out) {
+        String method = reqString.substring(0, reqString.indexOf(" "));
+        System.out.println(method);
+        if (method.equals("GET")) {
+            //header
+            out.println("HTTP/1.1 200 ok");
+            out.println("Content-Type: text/plain");
+            out.println();
+            //body
+            out.println("Allowed get request");
+        } else  {
+            out.println("HTTP/1.1 403 Forbidden");
+            out.println("Content-Type: text/plain");
+            out.println();
+            out.println("Not allowed request");
+        }
+    }
 
 }
